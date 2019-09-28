4de732366de0870b76a91c7bff26fb1bb0797643
Your Name <fidelomolo7@gmail.com>
2019-09-28 23:07:27 +0300

    first commit

---
+++ src/main/java/com/example/tibasmart/Edit_Account.java
@@ -0,0 +1,19 @@
+package com.example.tibasmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit__account);
+
+
+
+    }
+
+
+}
